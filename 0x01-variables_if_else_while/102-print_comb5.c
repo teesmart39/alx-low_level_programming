b20e141eb87f9f4a59c50ac4e4a820f8cb356d68
teesmart39 <okelolatemitopeoluwaseun@gmail.com>
2022-03-11 12:34:03 -0800

    sfhkkk

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main-Program entry point.
+ *Return:0-success, Non-zero, fail.
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i == '9' && j == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
